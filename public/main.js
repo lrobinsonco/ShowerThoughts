71936280ad6a546623ac8310f321e7cfbe0fc060
lrobinsonco <lrobinsonco@gmail.com>
2017-03-07 15:34:56 -0700

    put on fiebox

--- public/main.js
+++ public/main.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
   $('.btn').on('click', function () {
     getThought();
+    resetvalue();
   });
 });
 // everything happens inside of here, when the button is clicked
 function getThought(){
   // var randomVar = Math.floor(20)
   var randomVar = Math.floor((Math.random()*20));
-  console.log(randomVar)
+  console.log(randomVar);
 // remove anything in container
-   $('.advice').remove()
-   $('.form-container').remove()
+   $('.advice').remove();
+  //  $('.form-container').text("")
 var url = "https://galvanize-cors.herokuapp.com/https://www.reddit.com/r/Showerthoughts/new.json";
 $.get(url, function(data){
 // do we need data.data.title?qqq1q`1`111
@@ -25,6 +26,21 @@ $.get(url, function(data){
 function showTitle(title) {
   $(".advice-container").html('<span class="poop">'+title+'</span>');
 }
+
+function resetvalue() {
+  $(".validate").val("");
+}
+
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyB4F1Kk2bJuPxM0ry67kYIER-EQmVp9rwk",
+  //   authDomain: "roshambo-37199.firebaseapp.com",
+  //   databaseURL: "https://roshambo-37199.firebaseio.com",
+  //   storageBucket: "roshambo-37199.appspot.com",
+  //   messagingSenderId: "265232844958"
+  // };
+  // firebase.initializeApp(config);
+
 //   var settings = {
 //  "async": true,
 //  "crossDomain": true,
